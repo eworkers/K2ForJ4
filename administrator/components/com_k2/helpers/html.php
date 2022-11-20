d58a8001b0c8ebf3592c7ca0ef66c0ef176b3913
eworkers <eworkersdev@gmail.com>
2022-11-20 22:39:57 +0200

    a temp approach (override to k2.backend.css) to make views more readable and feel more like k2 like on j3

--- administrator/components/com_k2/helpers/html.php
+++ administrator/components/com_k2/helpers/html.php
@@ -371,6 +371,11 @@ class K2HelperHTML
                 if ($option == 'com_k2') {
                     $document->addStyleSheet('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
                     $document->addStyleSheet(JURI::root(true) . '/media/k2/assets/css/k2.backend.css?v=' . K2_CURRENT_VERSION . '&b=' . K2_BUILD_ID);
+                    if (version_compare(JVERSION, '4.0.0-dev', 'ge'))
+                    {
+
+                        $document->addStyleSheet(JURI::root(true) . '/media/k2/assets/css/k2.j4.backend.css?v=' . K2_CURRENT_VERSION . '&b=' . K2_BUILD_ID);
+                    }
                 }
                 if ($adminModuleIncludes) {
                     $document->addStyleSheet(JURI::root(true) . '/media/k2/assets/css/k2.global.css?v=' . K2_CURRENT_VERSION . '&b=' . K2_BUILD_ID);
