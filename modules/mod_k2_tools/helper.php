49a4f2cf7563dd3ab946d54d98a88e79ead9d4de
eworkers <115772544+eworkers@users.noreply.github.com>
2023-04-16 09:55:29 +0300

    farewell joomfish

--- modules/mod_k2_tools/helper.php
+++ modules/mod_k2_tools/helper.php
@@ -514,14 +514,7 @@ class modK2ToolsHelper
             <input name="option" value="com_k2" type="hidden" />
             <input name="view" value="itemlist" type="hidden" />
             <input name="task" value="category" type="hidden" />
-            <input name="Itemid" value="' . Factory::getApplication()->input->getInt('Itemid') . '" type="hidden" />';
-
-            // For Joom!Fish compatibility
-            if (Factory::getApplication()->input->getCmd('lang')) {
-                echo '<input name="lang" value="' . Factory::getApplication()->input->getCmd('lang') . '" type="hidden" />';
-            }
-
-            echo '
+            <input name="Itemid" value="' . $app->input->getInt('Itemid') . '" type="hidden" />
     </form>
 </div>
             ';
